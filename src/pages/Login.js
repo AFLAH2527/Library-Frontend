f3e7c14a24a24770ed735591cebddbeecfdcbafb
= <=>
2025-04-04 00:10:00 +0530

    styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,6 @@ import React, { useState, useContext } from "react";
 import { login } from "../services/authService";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
